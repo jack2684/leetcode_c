955f867a65fca6be9f6db906f18309a2c224fb67
jack2684 <gjj@cs.dartmouth.edu>
2014-01-11 09:57:19 -0500

    Evaluate Reverse Polish Notation
    // a better and easier solution is to use a stack, I didn't come up to
    that at first

---
+++ evaluateRPN.cpp
@@ -0,0 +1,48 @@
+// a better and easier solution is to use a stack, I didn't come up to that at first'
+class Solution {
+public:
+    int evalRPN(vector<string> &tokens) {
+        if(!tokens.size()) return 0;
+        if(tokens.size() == 1)
+            return stoi(tokens.front());
+            
+        string op;
+        int num1, num2;
+        op = tokens.back();
+        tokens.pop_back();
+        
+        int idx = tokens.size(), expectNum = 1;
+        vector<string>::iterator it = tokens.end();
+        vector<string> aux;
+        while(true){
+            it--;
+            idx--;
+            if(isOperator(tokens[idx])){
+                expectNum += 1;
+            }else if(--expectNum == 0){
+                aux.insert(aux.begin(), it, tokens.end());
+                tokens.erase(it, tokens.end());
+                num1 = evalRPN(tokens);
+                num2 = evalRPN(aux);
+                break;
+            }
+        }
+
+        if(op == "+"){
+            return num1 + num2;
+        }else if(op == "-"){
+            return num1 - num2;
+        }else if(op == "*"){
+            return num1 * num2;
+        }else if(op == "/"){
+            return num1 / num2;
+        }else{
+            cout << "wrong operator\n";
+        }
+    }
+
+    bool isOperator(string s){
+        return s == "+" || s == "-"
+            || s == "*" || s == "/";
+    }
+};
